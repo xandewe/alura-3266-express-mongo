30a961ab081395f1a2ce595b35d3c31bd940bb18
alexandre <alexandrealvescs@gmail.com>
2024-02-26 18:12:56 -0300

    fix: argument adjustment in populate method

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -4,7 +4,7 @@ import { autor } from "../models/autor.js";
 class LivroController {
     static async listarLivros (req, res) {
         try {
-            const listaLivros = await livro.find({}).populate("autores").exec();
+            const listaLivros = await livro.find({}).populate("autor").exec();
             res.status(200).json(listaLivros);
         } catch (erro) {
             res.status(500).json({message: `${erro.message} - falha na requisição`});
